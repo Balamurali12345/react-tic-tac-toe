d58362749e775bdc4f47c9e543f1ede931880aed
Balamurali12345 <167961569+Balamurali12345@users.noreply.github.com>
2024-05-01 11:21:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tic-tac-toe
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Balamurali12345/react-tic-tac-toe)
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+import Square from './Square';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+}
